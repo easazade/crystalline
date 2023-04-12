0f6465c75225b7ef394a26616eaec3b89573d2f1
alireza easazade <easazade@gmail.com>
2023-04-12 22:16:36 +0330

    chore(flutter_crystalline): Remove BuildableData since it serves no additional purpose than Data

--- packages/flutter_crystalline/lib/flutter_crystalline.dart
+++ packages/flutter_crystalline/lib/flutter_crystalline.dart
@@ -3,4 +3,3 @@ library flutter_crystalline;
 export 'package:crystalline/crystalline.dart';
 export 'src/builders.dart';
 export 'src/data.dart';
-export 'src/store.dart';
--- packages/flutter_crystalline/lib/src/data.dart
+++ packages/flutter_crystalline/lib/src/data.dart
@@ -1,44 +1,6 @@
 import 'package:crystalline/crystalline.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_crystalline/src/builders.dart';
-import 'package:flutter_crystalline/src/when_builders.dart';
 
-class BuildableData<T> extends Data<T> {
-  BuildableData({super.value, super.error, super.operation});
-
-  Widget build({
-    required final DataWidgetBuilder<T> builder,
-    bool listen = false,
-  }) =>
-      DataBuilder<T>(data: this, builder: builder, listen: listen);
-
-  Widget buildWhen({
-    required DataWidgetBuilder<T> onAvailable,
-    DataWidgetBuilder<T>? onNotAvailable,
-    DataWidgetBuilder<T>? onLoading,
-    DataWidgetBuilder<T>? onCreate,
-    DataWidgetBuilder<T>? onDelete,
-    DataWidgetBuilder<T>? onFetch,
-    DataWidgetBuilder<T>? onUpdate,
-    DataWidgetBuilder<T>? onError,
-    DataWidgetBuilder<T>? orElse,
-    bool listen = false,
-  }) =>
-      WhenDataBuilder<T>(
-        data: this,
-        onAvailable: onAvailable,
-        onNotAvailable: onNotAvailable,
-        onLoading: onLoading,
-        onCreate: onCreate,
-        onDelete: onDelete,
-        onFetch: onFetch,
-        onUpdate: onUpdate,
-        onError: onError,
-        orElse: orElse,
-        listen: listen,
-      );
-}
-
-class ChangeNotifierData<T> extends BuildableData<T>
+class ChangeNotifierData<T> extends Data<T>
     with ChangeNotifier
     implements ChangeNotifier {}
