e1968eed31861755cee6c0bc55688536d7eb5d81
alireza easazade <easazade@gmail.com>
2023-11-26 12:37:54 +0330

    chore(flutter_crystalline): Rename file data.dart to store.dart

--- packages/flutter_crystalline/lib/flutter_crystalline.dart
+++ packages/flutter_crystalline/lib/flutter_crystalline.dart
@@ -3,4 +3,4 @@ library flutter_crystalline;
 export 'package:crystalline/crystalline.dart';
 export 'src/builders.dart';
 export 'src/when_builders.dart';
-export 'src/data.dart';
+export 'src/store.dart';
