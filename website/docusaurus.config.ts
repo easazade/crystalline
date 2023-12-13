5a201ff5b310d0965dc122783576a553793c4861
alireza easazade <easazade@gmail.com>
2023-12-13 22:24:03 +0330

    chore(website): Update website title

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Crystalline',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -58,9 +58,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Crystalline',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Crystalline Logo',
         src: 'img/logo.svg',
       },
       items: [
