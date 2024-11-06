96ebdb4ab5f2079fd859b7f894f5106acdffbb41
alireza easazade <easazade@gmail.com>
2024-11-07 02:24:20 +0330

    Add more tests

--- packages/flutter_crystalline/test/store_builder_test.dart
+++ packages/flutter_crystalline/test/store_builder_test.dart
@@ -31,6 +31,76 @@ void main() {
       );
 
       expect(find.text(testStore.userName.value), findsOneWidget);
+      expect(find.text(testStore.nonData), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'Should rebuild the ui when publish() is called',
+    (tester) async {
+      await tester.pumpWidget(
+        Testable(
+          child: StoreBuilder(
+            store: testStore,
+            builder: (context, store, child) {
+              return Column(
+                children: [
+                  if (store.userName.hasValue) Text(store.userName.value),
+                  if (store.age.hasValue) Text('${store.age.value}'),
+                  if (store.points.hasValue) Text('${store.points.value}'),
+                ],
+              );
+            },
+          ),
+        ),
+      );
+
+      expect(find.text(testStore.userName.value), findsOneWidget);
+
+      testStore.userName.value = 'easazade';
+      testStore.age.value = 30;
+      testStore.points.value = 10.0;
+      testStore.publish();
+      await tester.pumpAndSettle();
+
+      expect(find.text('easazade'), findsOneWidget);
+      expect(find.text('30'), findsOneWidget);
+      expect(find.text('10.0'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'Should NOT rebuild the ui when publish() is called',
+    (tester) async {
+      await tester.pumpWidget(
+        Testable(
+          child: StoreBuilder(
+            store: testStore,
+            builder: (context, store, child) {
+              return Column(
+                children: [
+                  if (store.userName.hasValue) Text(store.userName.value),
+                  if (store.age.hasValue) Text('${store.age.value}'),
+                  if (store.points.hasValue) Text('${store.points.value}'),
+                ],
+              );
+            },
+          ),
+        ),
+      );
+
+      expect(find.text(testStore.userName.value), findsOneWidget);
+
+      testStore.userName.value = 'easazade';
+      testStore.age.value = 30;
+      testStore.points.value = 10.0;
+      // not calling publish
+      // testStore.publish();
+      await tester.pumpAndSettle();
+
+      expect(find.text('easazade'), findsNothing);
+      expect(find.text('30'), findsNothing);
+      expect(find.text('10.0'), findsNothing);
     },
   );
 }
