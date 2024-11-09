89ab1c72b0a24047c0a5cf11556eb7f336262320
alireza easazade <easazade@gmail.com>
2024-11-09 12:16:11 +0330

    feat(flutter_crystalline): Add more tests for WhenDataBinder

--- packages/flutter_crystalline/test/data_binder.dart/when_data_binder_test.dart
+++ packages/flutter_crystalline/test/data_binder.dart/when_data_binder_test.dart
@@ -81,10 +81,15 @@ void main() {
       expect(find.text('delete'), findsOneWidget);
       expect(find.text('operating'), findsNothing);
 
+      data.operation = Operation.update;
+      await tester.pumpAndSettle();
+      expect(find.text('update'), findsOneWidget);
+      expect(find.text('delete'), findsNothing);
+
       data.operation = Operation('like-photo');
       await tester.pumpAndSettle();
       expect(find.text('like-photo'), findsOneWidget);
-      expect(find.text('update'), findsNothing);
+      expect(find.text('delete'), findsNothing);
 
       data.operation = Operation.none;
       data.failure = Failure('oops error !!!');
@@ -116,6 +121,97 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(find.text('update-profile-image'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'orElse callback should be called when the relevant callback for current state of '
+    'Data is not implemented.',
+    (tester) async {
+      await tester.pumpWidget(
+        Testable(
+          child: WhenDataBinder(
+            data: data,
+            onValue: (context, data) => Text(data.value),
+            orElse: (context, data) => Text('or else'),
+          ),
+        ),
+      );
+
+      expect(find.text('or else'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'fallback widget should be shown when the relevant callback for current state of '
+    'Data is not implemented. and orElse is not implemented either.',
+    (tester) async {
+      await tester.pumpWidget(
+        Testable(
+          child: WhenDataBinder(
+            data: data,
+            onValue: (context, data) => Text(data.value),
+            fallback: Center(),
+          ),
+        ),
+      );
+
+      expect(find.byType(Center), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'Should observe data and update builder when data updated '
+    'then when data objects changes should remove observer from old data '
+    'and observe the new data instead.',
+    (tester) async {
+      final data1 = Data<String>();
+      final data2 = Data<String>();
+      // when data is set to data1;
+      Data<String> Function() getData = () => data1;
+
+      late Function rebuild;
+
+      await tester.pumpWidget(
+        StatefulBuilder(builder: (context, setState) {
+          rebuild = () => setState(() {});
+
+          return Testable(
+            child: WhenDataBinder(
+              data: getData(),
+              onValue: (context, data) => Text(data.value),
+              onNoValue: (context, data) => Container(),
+            ),
+          );
+        }),
+      );
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(Container), findsOneWidget);
+      expect(find.text('text'), findsNothing);
+
+      // when data1 value changes
+      data1.value = 'text';
+
+      await tester.pumpAndSettle();
+
+      // expect changes from data1
+      expect(find.text('text'), findsOneWidget);
+      expect(find.byType(Container), findsNothing);
+
+      // when data object is changed from data1 to data2
+      getData = () => data2;
+      rebuild();
+
+      // and when data2 value changes
+      data2.value = 'new value';
+
+      await tester.pumpAndSettle();
+
+      // expect changes on widget from data2
+      expect(find.text('new value'), findsOneWidget);
+      expect(find.byType(Container), findsNothing);
     },
   );
 }
