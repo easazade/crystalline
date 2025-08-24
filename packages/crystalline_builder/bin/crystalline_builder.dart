3f92535f252d1be8c5ddd74b87365d8ceb6d2272
Ali <easazade@gmail.com>
2025-08-24 11:04:30 +0330

    Add new package weaver_builder

---
+++ packages/crystalline_builder/bin/crystalline_builder.dart
@@ -0,0 +1,5 @@
+import 'package:crystalline_builder/crystalline_builder.dart' as crystalline_builder;
+
+void main(List<String> arguments) {
+  print('Hello world: ${crystalline_builder.calculate()}!');
+}
---
+++ packages/crystalline_builder/test/crystalline_builder_test.dart
@@ -0,0 +1,8 @@
+import 'package:crystalline_builder/crystalline_builder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
